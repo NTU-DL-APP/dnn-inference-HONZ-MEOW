cfa67f1aad6ababc2a57e90cc4724148acf3e9f7
HONZ-MEOW <goodgood32323@gmail.com>
2025-06-10 14:37:23 +0800

    fix4

--- nn_predict.py
+++ nn_predict.py
@@ -6,10 +6,14 @@ def relu(x):
     return np.maximum(0, x)
 
 def softmax(x):
-    x = x - np.max(x, axis=1, keepdims=True)
-    e_x = np.exp(x).astype(np.float32)
-    out = e_x / np.sum(e_x, axis=1, keepdims=True)
-    return np.round(out, decimals=6)  # 保留 6 位小數避免浮點誤差
+    if x.ndim == 1:
+        x = x - np.max(x)
+        e_x = np.exp(x)
+        return e_x / np.sum(e_x)
+    else:
+        x = x - np.max(x, axis=1, keepdims=True)
+        e_x = np.exp(x)
+        return e_x / np.sum(e_x, axis=1, keepdims=True)
 
 
 # === Flatten ===
